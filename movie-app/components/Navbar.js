fe712db98a0856294ed9b1d53d9870f8a39d047e
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-01-22 14:54:07 +0300

    create the navbar component and request api

--- movie-app/components/Navbar.js
+++ movie-app/components/Navbar.js
@@ -1,7 +1,14 @@
-
+import requests from "@/utils/requests"
+import { useRouter } from "next/router"
 
 export const Navbar = () => {
+
+    const router = useRouter()
   return (
-    <div>Navbar</div>
+    <div className="flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl ">
+         {Object.entries(requests).map(([key, { title, url }]) => (
+        <h2 onClick={()=>(router.push(`?genre=${key}`))} key={key} className="m-6 cursor-pointer hover:text-white active:text-red-400">{title}</h2>
+        ))}
+    </div>
   )
 }
