4374948886c1c9039f887024f2edcf3794185dca
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-01-22 12:05:40 +0300

    finish styling of header section

--- movie-app/components/Header.js
+++ movie-app/components/Header.js
@@ -4,9 +4,9 @@ import { HomeIcon, UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/r
 
 export default function Header() {
   return (
-    <div>
+    <div className="bg-gray-700 text-gray-200 flex flex-col  items-center p-6 select-none sm:flex-row justify-between">
 
-        <div className="">
+        <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
@@ -15,6 +15,7 @@ export default function Header() {
 
 <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" 
 width={100} height={100}
+className=" cursor-pointer active:brightness-110"
         />
     </div>
   )
